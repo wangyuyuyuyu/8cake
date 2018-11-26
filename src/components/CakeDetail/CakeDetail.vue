05cf2703fa73656d19221f0b10543da3f40bfcf0
cheng <42559607+stargazerkiyoshi@users.noreply.github.com>
2018-11-26 19:04:47 +0800

    add directors for component

---
+++ src/components/CakeDetail/CakeDetail.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "CakeDetail"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/ServiceIntroduce/ServiceIntroduce.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "ServiceIntroduce"
+    }
+</script>
+
+<style scoped>
+
+</style>
