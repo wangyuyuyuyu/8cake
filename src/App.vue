14d65b3932cac2b3a67dc4cb53a52729fee7a1f2
“zhengsheng <1832070321@qq.com>
2018-11-27 14:10:09 +0800

    add

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,20 @@
 <template>
   <div id="app">
     <router-view/>
-
+    <Login></Login>
   </div>
 </template>
 
 <script>
-
+import Login from './components/Login/Login'
 export default {
   name: 'App',
   components:{
-
+    Login
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
